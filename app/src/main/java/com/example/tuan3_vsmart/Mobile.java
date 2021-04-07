08633d850cc21a3369e3a401bb6ecfe7341620d4
YtalyPham <nhuyphamdev@gmail.com>
2021-04-07 20:43:02 +0700

    first commit

---
+++ app/src/main/java/com/example/tuan3_vsmart/Mobile.java
@@ -0,0 +1,19 @@
+package com.example.tuan3_vsmart;
+
+public class Mobile {
+    private String name;
+    private  String color;
+    private String producer;
+    private String price;
+    private  int img;
+
+    public Mobile(String name, String color, String producer, String price, int img) {
+        this.name = name;
+        this.color = color;
+        this.producer = producer;
+        this.price = price;
+        this.img = img;
+    }
+
+
+}
